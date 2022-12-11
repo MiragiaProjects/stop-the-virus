2e2567aa453ccba84d8803203cbcaa667affc0b9
Malin Olsson <miragia.solinde@gmail.com>
2022-12-11 18:00:07 +0100

    for when a player joins

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -11,3 +11,55 @@ let queue = 0;
 let waitingTime = 0;       
 
 let virusPlace = null;    
+
+// User joining
+const userJoined = function(username, callback) {
+
+	let startGame = false;
+	
+	if (queue === 0) {
+		rooms.push({
+			room_id: numberOfRooms++,
+			numberOfPlayers: 0,
+			status: 'queue', 
+			players: {},
+			playedRounds: 0, 
+		});
+	}
+
+	const currentRoom = rooms[rooms.length - 1];
+	if (currentRoom.numberOfPlayers > 0 && Object.values(currentRoom.players)[0].username === username) {
+		callback({
+			success: false,
+			msg: 'Username already taken.',
+		});
+		return;
+	}
+
+	currentRoom.numberOfPlayers++;
+	this.join(currentRoom);
+	currentRoom.players[this.id] = {
+		username,
+		points: 0,
+	 	previousReactionTime: null,
+	};
+	
+	queue++;
+	
+	if (queue === 2) {
+		queue = 0;
+
+		currentRoom.status = 'started',
+		startGame = true;
+
+		timeAndPosition();
+		io.in(currentRoom).emit('print:names', currentRoom.players);
+		this.broadcast.to(currentRoom).emit('game:start', waitingTime, virusPlace);
+	}
+	callback({
+		success: true,
+		startGame,
+		waitingTime,
+		virusPlace, 
+	});
+}
